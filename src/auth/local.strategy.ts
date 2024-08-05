ac36d9c0cc49f923f859c819885d873afc48fb94
anonruff <jamaludinahmad013@gmail.com>
2024-08-05 07:16:40 +0700

    feat(auth): error handling

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,5 +1,5 @@
 import {Strategy} from 'passport-local';
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { Injectable, NotFoundException, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { AuthService } from './auth.service';
 
@@ -13,7 +13,7 @@ export class LocalStrategy extends PassportStrategy(Strategy){
         const user = await this.authService.validateUser(username, password)   
             
         if (!user) {
-            throw new UnauthorizedException();
+            throw new NotFoundException();
         }
         return user;
     }
